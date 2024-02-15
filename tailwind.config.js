2ad84531221fce7359c1972643021d13448d38b9
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-15 14:08:38 +0530

    design: register-form

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        darkBlue: '#265073',
+        midBlue: '#2D9596',
+        lightMidBlue: '#9AD0C2',
+        lightGrey: '#F1FADA',
+      }
+    },
+  },
+  plugins: [],
+}
+
