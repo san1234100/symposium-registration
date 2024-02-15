2bbb86a12d183ca01678f8533d4ccd35db3368ea
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-15 14:54:49 +0530

    feat: added data to local storage

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const formEl=document.querySelector('form');
+// console.log(formEl);
+const localStorageKey='user';
+
+const setandGetData=(e)=>{
+    e.preventDefault();
+    const formData=new FormData(formEl);
+    const userData=Object.fromEntries(formData);
+    const existingUserArr=JSON.parse(localStorage.getItem(localStorageKey));
+    if(existingUserArr){
+    existingUserArr.push(userData)
+    localStorage.setItem(localStorageKey,JSON.stringify(existingUserArr))
+    console.log(existingUserArr);
+    } else{
+      const newArr=[];
+      newArr.push(userData);
+      localStorage.setItem(localStorageKey,JSON.stringify(newArr))
+    }
+}
+
+
+formEl.addEventListener('submit',setandGetData)
